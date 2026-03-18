0e27a73e02c18b5a6a0dd4ac74f82dd7756e3697
mateussfragaa <mateusflofraga@gmail.com>
2026-03-18 00:05:22 -0300

    Aprendendo módulos em NodeJS

---
+++ calc.js
@@ -0,0 +1,9 @@
+const soma = require("./funcoes_calc/soma");
+const subt = require("./funcoes_calc/mult");
+const mult = require("./funcoes_calc/subt");
+const divi = require("./funcoes_calc/divi");
+
+console.log(soma(10, 2));
+console.log(subt(10, 2));
+console.log(mult(10, 2));
+console.log(divi(10, 2));
---
+++ funcoes_calc/divi.js
@@ -0,0 +1,5 @@
+const divi = function(a, b){
+    return a / b;
+};
+
+module.exports = divi;
